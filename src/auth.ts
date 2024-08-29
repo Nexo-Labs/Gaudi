e0f5d2395c0b165ac3c823b3bbb3c69c775d9461
Rubén <ruben@nexolabs.xyz>
2024-08-30 00:59:39 +0200

    feat: Set environemnt variables as public and dynamic but not working with docker yet

--- src/auth.ts
+++ src/auth.ts
@@ -1,18 +1,13 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import Keycloak from "@auth/sveltekit/providers/keycloak";
-import {
-  AUTH_KEYCLOAK_ID,
-  AUTH_KEYCLOAK_ISSUER,
-  AUTH_KEYCLOAK_SECRET,
-  AUTH_SECRET,
-} from "$env/static/private";
+import { env } from '$env/dynamic/public'
 
-const authjsSecret = AUTH_SECRET; // Use Environment Variables AUTH_SECRET in prod
+const authjsSecret = env.PUBLIC_AUTH_SECRET; // Use Environment Variables AUTH_SECRET in prod
 
 const kcConfig = {
-  issuer: AUTH_KEYCLOAK_ISSUER, // Use Environment Variables AUTH_KEYCLOAK_ISSUER in prod
-  clientId: AUTH_KEYCLOAK_ID, // Paste "Client id" here. Use Environment Variables AUTH_KEYCLOAK_ID in prod
-  clientSecret: AUTH_KEYCLOAK_SECRET, // Paste "Client secret" here. Use Environment Variables AUTH_KEYCLOAK_ISSUER in prod
+  issuer: env.PUBLIC_AUTH_KEYCLOAK_ISSUER, // Use Environment Variables AUTH_KEYCLOAK_ISSUER in prod
+  clientId: env.PUBLIC_AUTH_KEYCLOAK_ID, // Paste "Client id" here. Use Environment Variables AUTH_KEYCLOAK_ID in prod
+  clientSecret: env.PUBLIC_AUTH_KEYCLOAK_SECRET, // Paste "Client secret" here. Use Environment Variables AUTH_KEYCLOAK_ISSUER in prod
 };
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
