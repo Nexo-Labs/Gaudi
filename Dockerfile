1b9f53662f9f219d44466ecef3cf792187ab0c48
Rubén <ruben@nexolabs.xyz>
2024-08-30 10:05:31 +0200

    feat: Entrypoint added

--- Dockerfile
+++ Dockerfile
@@ -9,4 +9,5 @@ COPY . ./
 
 RUN yarn build
 
+ENTRYPOINT [ "/app/scripts/entrypoint.sh" ]
 CMD ["sh", "-c", "node --env-file=/app/.env build"]
---
+++ scripts/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Crear el archivo .env
+touch /app/.env
+
+# Iterar sobre todas las variables de entorno y guardarlas en el archivo .env
+for var in $(printenv)
+do
+  # Formato esperado: VAR_NAME=value
+  # Filtrar la variable para evitar que algunas variables sensibles de Docker se guarden.
+  if echo "$var" | grep -q -v '^PWD=\|^SHLVL=\|^_=.\|^OLDPWD=\|^HOME=\|^HOSTNAME=\|^PATH=\|^TERM='; then
+    echo "$var" >> /app/.env
+  fi
+done
+
+# Ejecutar el comando que se pase como argumento
+exec "$@"
