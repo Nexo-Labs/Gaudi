c19f1426ea82b366e92aded68d07e4dbb555ca64
Rubén <ruben@nexolabs.xyz>
2024-09-01 22:12:42 +0200

    spike: First approach of stripe working

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,31 @@
-FROM node:latest
+FROM node:latest AS Dependencies
 
 WORKDIR /app
 
-COPY package.json ./
-COPY yarn.lock ./
+COPY package.json yarn.lock ./
 RUN yarn
-COPY . ./
 
+
+FROM node:latest AS Build
+WORKDIR /app
+
+COPY --from=Dependencies /app/node_modules ./node_modules
+COPY . .
+
+RUN npx prisma generate
 RUN yarn build
+
+FROM node:latest AS Deploy
+WORKDIR /app
+
+ENV NODE_ENV production
+COPY --from=build /app/package.json package.json
+COPY --from=build /app/.svelte-kit .svelte-kit
+COPY --from=build /app/build build
+COPY --from=build /app/static static
+COPY --from=build /app/scripts/entrypoint.sh scripts/entrypoint.sh
 RUN chmod +x /app/scripts/entrypoint.sh
+
 
 ENTRYPOINT [ "/app/scripts/entrypoint.sh" ]
 CMD ["sh", "-c", "node --env-file=/app/.env build"]
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,44 @@
 	import type { LayoutServerData } from "./$types.js";
 	import { flatMap } from "$src/domain/common/Optional.js";
 	import { mapSessionToUserModel } from "$src/domain/user-model.js";
+	import { cartItems, addToCart, removeFromCart } from "$src/cart.js";
 
 	export let data: LayoutServerData;
 
 	let userModel = flatMap(data?.session, session => mapSessionToUserModel(session))
+
+	const products = [
+		{
+			id: 1,
+			name: "Dog",
+			price: 14.99,
+		},
+		{
+			id: 2,
+			name: "Cat",
+			price: 14.99,
+		},
+		{
+			id: 3,
+			name: "Fish",
+			price: 14.99,
+		},
+	];
+
+	async function checkout() {
+		const data = await fetch(
+			"/checkout", 
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({items: $cartItems})
+			}
+		)
+		.then((data) => data.json());
+		window.location.replace(data.url);
+	}
 
 </script>
 
@@ -17,9 +51,35 @@
 
 <section>
 	<MainSection></MainSection>
-	<div class="py-12">
-		<div class="mx-auto max-w-3xl px-6 lg:px-8 text-center">
-          
+	<h1>Products</h1>
+
+	<div class="grid">
+	  {#each products as product}
+		<div>
+		  <h2>{product.name}</h2>
+		  <p>Price: ${product.price}</p>
+		  <button on:click={() => addToCart(product)}>Add to cart</button>
 		</div>
+	  {/each}
+	</div>
+  
+	{#if $cartItems.length > 0}
+	  <h1>Cart</h1>
+  
+	  <div class="grid">
+		{#each $cartItems as cartItem}
+		  <div>
+			<h2>{cartItem.name}</h2>
+			<p>Amount: {cartItem.amount}</p>
+			<p>Price: ${cartItem.price}</p>
+			<p>Total: ${cartItem.price * cartItem.amount}</p>
+			<button on:click={() => removeFromCart(cartItem.id)}
+			  >Remove from cart</button
+			>
+		  </div>
+		{/each}
 	  </div>
+	{/if}
+  
+	<button on:click={checkout}>Checkout</button>
 </section>
