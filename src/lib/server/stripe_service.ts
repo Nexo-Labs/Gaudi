b7c7a4a3916e16e74563213f8e6792f5a5fcd74f
Rubén <ruben@nexolabs.xyz>
2024-09-10 01:19:34 +0200

    feat: Strapi webhooks is working

--- src/lib/server/stripe_service.ts
+++ src/lib/server/stripe_service.ts
@@ -1,7 +1,7 @@
 import Stripe from 'stripe'
 import { env } from '$env/dynamic/private'
 
-const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
   apiVersion: '2024-06-20'
 })
 
---
+++ src/routes/webhook/stripe/+server.ts
@@ -0,0 +1,28 @@
+import { error, json } from '@sveltejs/kit';
+import type Stripe from 'stripe';
+import { env } from '$env/dynamic/private';
+import { stripe } from '$src/lib/server/stripe_service.js';
+
+export const POST = async ({ request }) => {
+	const whSecret = env.STRIPE_WEBHOOK_SECRET;
+	const body = await request.text();
+
+	const signature = request.headers.get('stripe-signature') ?? '';
+
+	try {
+		const event = stripe.webhooks.constructEvent(body, signature, whSecret);
+
+		if (event.type === 'checkout.session.completed') {
+			const sessionWithCustomer = await stripe.checkout.sessions.retrieve(event.data.object.id, {
+				expand: ['customer']
+			});
+
+			const customer = sessionWithCustomer.customer as Stripe.Customer;
+		}
+	} catch (err) {
+		console.log('Something went wrong.', err);
+		error(500);
+	}
+
+	return json({ success: true });
+};
