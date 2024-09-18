4bcdd3c4ad36ed2138457301f869de03e211d9ce
Rubén <ruben@nexolabs.xyz>
2024-09-19 00:24:00 +0200

    fix: complex error due to loading prisma

--- src/lib/domain/user-model.ts
+++ src/lib/domain/user-model.ts
@@ -1,9 +1,7 @@
 import { jwtDecode } from 'jwt-decode';
 import type { Session } from '@auth/sveltekit';
-import { SubscriptionStatus, type Prisma } from '@prisma/client';
 import { flatMap, type Optional } from './common/Optional.js';
 
-type UsersPrismaModel = Prisma.UserGetPayload<{}>
 
 export interface UserModel {
 	userId: string;
@@ -11,24 +9,4 @@ export interface UserModel {
 	image: Optional<string>;
 	email: string;
 	roles: string[];
-}
-
-export function mapSessionToUserModel(session: Session): UserModel | undefined {
-	const user = session.user as UsersPrismaModel | null;
-	if (!user || user.email == null) return undefined;
-
-	const activeSubscription = user.subscriptionStatus?.includes(SubscriptionStatus.ACTIVE) ?? false;
-
-	let roles: string[] = [];
-	if (activeSubscription && user.priceId) {
-		roles.push(user.priceId);
-	}
-
-	return {
-		userId: user.id,
-		name: user.name,
-		email: user.email,
-		image: user.image,
-		roles: roles
-	};
 }
--- src/lib/server/auth.service.ts
+++ src/lib/server/auth.service.ts
@@ -1,14 +1,15 @@
-import { SvelteKitAuth } from '@auth/sveltekit';
+import { SvelteKitAuth, type Session } from '@auth/sveltekit';
 import Keycloak from '@auth/sveltekit/providers/keycloak';
 import { env } from '$env/dynamic/public';
 import { redirect } from '@sveltejs/kit';
 import { flatMap, type Optional } from '../domain/common/Optional.js';
-import { type UserModel, mapSessionToUserModel } from '../domain/user-model.js';
+import { type UserModel } from '../domain/user-model.js';
 import { externalUrl } from './routing.js';
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prismaClient } from './prisma/prisma_client.js';
 import { getUserInfo } from './keycloak.service.js';
 import { updateUserRoles } from './prisma/update_user_roles.js';
+import { SubscriptionStatus, type Prisma } from '@prisma/client';
 
 const authjsSecret = env.PUBLIC_AUTH_SECRET;
 
@@ -44,4 +45,27 @@ export async function restrictAuth(locals: App.Locals): Promise<UserModel> {
 	const user = await getUser(locals);
 	if (!user) return redirect(303, '/');
 	return user;
+}
+
+
+type UsersPrismaModel = Prisma.UserGetPayload<{}>
+
+export function mapSessionToUserModel(session: Session): UserModel | undefined {
+	const user = session.user as UsersPrismaModel | null;
+	if (!user || user.email == null) return undefined;
+  
+	const activeSubscription = user.subscriptionStatus?.includes("ACTIVE") ?? false;
+
+	let roles: string[] = [];
+	if (activeSubscription && user.priceId) {
+		roles.push(user.priceId);
+	}
+
+	return {
+		userId: user.id,
+		name: user.name,
+		email: user.email,
+		image: user.image,
+		roles: roles
+	};
 }
